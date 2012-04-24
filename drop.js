96e33463f3e0f804e4762a9cd21e8a58616effc2
Johannes Lindén <pj-linden@hotmail.com>
2012-04-24 07:51:45 +0200

    Improvments, with multiple files added support

--- drop.js
+++ drop.js
@@ -2,14 +2,14 @@
  * drop.js v0.4
  * Copyright 2012, Johan Lindskogen
  *
- * Useage: drop( element, callback(fileData) )
+ * Useage: drop( element, callBack(fileData) )
  * fileData.read will contain a string with the fileData
  * The element will get the "drop-zone"-class when hovering with a file.
  * 
  */
 
-function drop(zone, callback) {
-	var fileData = {};
+var drop = function(zone, callBack, fullCallBack) {
+	var fileData = [];
 	function cancel(event) {
 		event.stopPropagation();
 		event.preventDefault();
@@ -18,6 +18,18 @@ function drop(zone, callback) {
 		cancel(event);
 		zone.classList.toggle('drop-zone');
 	}
+	function readFile(file) {
+		var reader = new FileReader();
+		reader.onload = function(event) {
+			fileData[fileData.length-1].read = event.target.result;
+			callBack(fileData[fileData.length-1]);
+		}
+		if (file.type.indexOf('image') != -1)
+			reader.readAsDataURL(file);
+		else
+			reader.readAsText(file);
+	}
+
 	zone.addEventListener('dragenter', toggleCancel, false);
 	zone.addEventListener('dragover', cancel, false);
 	zone.addEventListener('dragleave', toggleCancel, false);
@@ -25,26 +37,17 @@ function drop(zone, callback) {
 		toggleCancel(event);
 		var files = event.dataTransfer.files;
 		for (var i = 0, len = files.length; i < len; i++){
-			fileData = {
+			fileData.push({
 				name: files[i].name,
 				size: files[i].size,
 				date: files[i].lastModifiedDate,
 				type: files[i].type,
 				originalFile: files[i]
-			};
+			});
 			readFile(files[i]);
 		}
+		if (typeof fullCallBack == 'function')
+			fullCallBack(fileData);
+
 	}, false);
-	function readFile(file) {
-		var reader = new FileReader();
-		reader.onload = handleResult;
-		if (file.type.indexOf('image') != -1)
-			reader.readAsDataURL(file);
-		else
-			reader.readAsText(file);
-	}
-	function handleResult(event) {
-		fileData.read = event.target.result;
-		callback(fileData);
-	}
 }
